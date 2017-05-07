ac243b845cd222a9e8b19a980773f9e7d9f719c6
jairogp <jairogp@users.noreply.github.com>
2017-05-07 05:40:18 -0300

    Add files via upload

---
+++ Exemplo_Controle_de_2_Servos_Motores/Exemplo_Controle_de_2_Servos_Motores.ino
@@ -0,0 +1,31 @@
+// Controle de 2 servos
+
+  #include <Servo.h>
+
+  Servo servo1;
+  Servo servo2;
+
+  void setup()
+{
+  servo1.attach(3);
+  servo2.attach(4);
+}
+
+void loop()
+{
+  servo1.write(10);
+  delay (1000);
+  servo1.write(180);
+  delay (1000);
+  servo2.write(10);
+  delay (1000);
+  servo2.write(180);
+  delay (1000);
+  servo1.write(10);
+  servo2.write(10);
+  delay (1000);
+  servo1.write(180);
+  servo2.write(180);
+  delay (1000);  
+}
+
